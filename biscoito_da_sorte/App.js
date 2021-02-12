9a33871b27bc35bfff8f9ce2772bde64b8f02cb4
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2021-02-12 19:41:10 -0300

    App Pronto

---
+++ biscoito_da_sorte/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
+import Index from './src/Index'
+
+export default function App() {
+  return (
+      <Index/>
+  );
+}
+
